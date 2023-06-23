9a9af05042e9525f83a3b856456426c1c9a452c9
zerx02 <137518199+zerx02@users.noreply.github.com>
2023-06-23 17:36:28 +0200

    Add files via upload

--- skrypt.sh
+++ skrypt.sh
@@ -1,55 +1,57 @@
 #!/bin/bash
 
-print_date() {
-  date
-}
-
-create_gitignore() {
-  echo "*log*" > .gitignore
-}
-
-create_logs() {
-  for ((i=1; i<=100; i++)); do
-    echo "Nazwa pliku: log${i}.txt" > log${i}.txt
-    echo "Nazwa skryptu: $0" >> log${i}.txt
-    echo "Data: $(date)" >> log${i}.txt
-  done
-}
-
-create_logs_with_count() {
-  local count=$1
-  for ((i=1; i<=count; i++)); do
-    echo "Nazwa pliku: log${i}.txt" > log${i}.txt
-    echo "Nazwa skryptu: $0" >> log${i}.txt
-    echo "Data: $(date)" >> log${i}.txt
-  done
-}
-
-show_help() {
-  echo "Dostępne opcje:"
-  echo "--date : Wyświetla dzisiejszą datę"
-  echo "--logs : Tworzy 100 plików logx.txt"
-  echo "--logs <liczba> : Tworzy określoną liczbę plików logx.txt"
-  echo "--help : Wyświetla pomoc"
-}
-
-if [[ $# -eq 0 ]]; then
-  echo "Nie podano żadnej flagi. Użyj --help, aby zobaczyć dostępne opcje."
-  exit 1
-fi
-
-if [[ "$1" == "--date" ]]; then
-  print_date
-fi
-
-if [[ "$1" == "--logs" ]]; then
-  if [[ "$2" =~ ^[0-9]+$ ]]; then
-    create_logs_with_count "$2"
-  else
-    create_logs
-  fi
-fi
-
-if [[ "$1" == "--help" ]]; then
-  show_help
-fi
+case "$1" in
+  --help | -h)
+    echo "Dostępne opcje:"
+    echo "--date,-d - wyświetla dzisiejszą datę"
+    echo "--logs,-l - tworzy pliki logx.txt"
+    echo "--help,-h - wyświetla wszystkie dostępne opcje"
+    echo "--init - klonuje repozytorium do bieżącego katalogu i ustawia ścieżkę w zmiennej PATH"
+    echo "--error,-e [liczba] - tworzy pliki errorx/errorx.txt"
+    shift
+    ;;
+  --logs | -l)
+    num_files=$2
+    for ((i=1; i<=num_files; i++)); do
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
+    done
+    shift
+    ;;
+  --date | -d)
+    echo $(date)
+    shift
+    ;;
+  --init)
+   
+    git clone https://github.com/zerx02/lab4
+    if [[ ":$PATH:" != *":$(pwd):"* ]]; then
+        export PATH=$PATH:$(pwd)
+    fi
+    shift
+    ;;
+  --error | -e)
+    if [[ -z $2 ]]; then
+      for ((i=1; i<=100; i++)); do
+        echo "Plik error$i.txt" > "error$i/error$i.txt"
+      done
+    else
+      num_files=$2
+      for ((i=1; i<=num_files; i++)); do
+        echo "Plik error$i.txt" > "error$i/error$i.txt"
+      done
+    fi
+    shift
+    ;;
+    --rebase)
+    branch_name=$2
+    git rebase "$branch_name"
+    shift
+    ;;
+    --merge)
+    branch_name=$2
+    git merge "$branch_name"
+    shift
+;;
+esac   
